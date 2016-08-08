16b87b239d287c7e5dd29e86b7bb58ee802cb3a2
Yiting <yiting.yu.tw@gmail.com>
2016-08-08 12:12:48 -0700

    modify corr.R

--- 02-R-Programming/Air_Pollution/corr.R
+++ 02-R-Programming/Air_Pollution/corr.R
@@ -3,31 +3,24 @@ corr <- function(directory, threshold = 0) {
   # List all csv in the directory
   filenames <- list.files(path = directory, pattern = "*.csv", full.names = TRUE)
   
-  # Ceeate an empty vector to store correalions 
-  correlation.vec <- vector()
+  # Ceeate an empty vector to store correlations 
+  cor.vec <- vector(mode = "numeric")
   
-  lapply(filenames, 
-         function(x){
-              # Read each csv and generate data.frame
-              each.df <- read.csv(x)
-              
-              # Create a vector to save whether it is a complete case or not
-              complete.index <- complete.cases(each.df)
-              
-              # Calcualte the number of complete case in each data.frame
-              nobs <- sum(complete.index) 
-              
-              # Compare it with threshold
-              if (nobs > threshold) {
-                  
-                  # Remove imcomplete cases and calculate correlations
-                  each.df <- each.df[complete.index, ]
-                  correlation.vec <<- c(correlation.vec, cor(each.df$sulfate, each.df$nitrate))
-              }
-              
-         })
+  # Loop through each file
+  for (i in 1:length(filenames)) {
+     
+    # Read each csv 
+    each.df <- read.csv(filenames[i])
+    
+    # Calculate number of complete cases
+    nobs <- sum(complete.cases(each.df))
+    
+    # If nobs > threshold, calculate correlatin of each df
+    if (nobs > threshold) {
+          each.df <- each.df[complete.cases(each.df), ]
+          cor.vec <- c(cor.vec, cor(each.df$sulfate, each.df$nitrate))
+          }
+    }
 
-  correlation.vec
-
-  
+  cor.vec
 }
